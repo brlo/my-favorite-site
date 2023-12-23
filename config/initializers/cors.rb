e68cd6b40276af6a23c0d15c4a09ff3062a91274
Rodion Vlasov <r.mk834@ya.ru>
2023-12-23 11:37:00 +0300

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,15 @@
 # EXAMPLES: https://github.com/cyu/rack-cors#rails-configuration
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    # origins '*'
+    # resource '*',
+    #   headers: :any,
+    #   methods: [:get, :post, :put, :patch, :delete, :options, :head]
 
+    origins 'https://bibleox.com', 'https://edit.bibleox.com', 'http://localhost:5173'
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      credentials: true,
+      methods: [ :get, :post, :put, :delete, :options ]
   end
 end
