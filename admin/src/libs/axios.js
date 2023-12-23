84271b061a4f655c0a4ea6a3385f70e1f7f418c0
Rodion Vlasov <r.mk834@ya.ru>
2023-12-23 12:29:37 +0300

    fix login api

--- admin/src/libs/axios.js
+++ admin/src/libs/axios.js
@@ -3,7 +3,7 @@ import { getCookie } from './cookies'
 
 // console.log('import.meta.env.VITE_API_URL', import.meta.env.VITE_API_URL)
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // задаётся в .env.local (local не уходит в git)
+  baseURL: import.meta.env.VITE_API_URL + '/ru/api/', // задаётся в .env.local (local не уходит в git)
   timeout: 1000,
   headers: {'X-API-TOKEN': getCookie('api_token')},
 })
