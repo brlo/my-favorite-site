928430cc833c3c5f355ba8ec280660b45e063e06
Rodion Vlasov <r.mk834@ya.ru>
2024-11-22 21:06:02 +0300

    fix notif position

--- app/assets/javascript/index.js
+++ app/assets/javascript/index.js
@@ -24,14 +24,14 @@ window.BX.notifications.show = function () {
   body.appendChild(el);
 
   // через 2 сек - затухаем
-  setTimeout(() => document.getElementById('popup-notif').style.opacity = '0', 2000);
+  setTimeout(() => document.getElementById('popup-notif').style.opacity = '0', 200000);
   // через 2.5 сек - исчезаем и рисуем следующее уведомлене
   setTimeout(function() {
     el = document.getElementById('popup-notif');
     body = document.getElementsByTagName('body')[0];
     body.removeChild(el);
     BX.notifications.show();
-  }, 2500);
+  }, 250000);
 }
 
 // Добавление уведомления в очередь на показ
