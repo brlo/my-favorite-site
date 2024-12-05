1117ee3a36b6ad0cf6a85ba20757c77ee60512b5
Rodion Vlasov <r.mk834@ya.ru>
2024-12-05 16:48:09 +0300

    increase simple image size

--- app/uploaders/simple_uploader.rb
+++ app/uploaders/simple_uploader.rb
@@ -2,7 +2,7 @@ require_relative "base_uploader"
 class SimpleUploader < BaseUploader
 
   version :m do
-    process :resize_to_fit => [1200, 800]
+    process :resize_to_fit => [1600, 1200]
   end
 
   version :s, :from_version => :m do
