ca99a2abb29c7155aedf34aa08130cb541809852
Rodion Vlasov <r.mk834@ya.ru>
2025-07-26 22:45:45 +0300

    fix fallback map

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -3,12 +3,17 @@ require "i18n/backend/fallbacks"
 I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
 
 I18n.available_locales = ::ALL_LOCALES.keys
-# %i(
-#   ar cn de en gr il jp ru
-#   es fr in ir it ke kr rs tm tr uz vn cp
-#)
 
-# I18n.default_locale = :ru
-::I18n.default_locale = :en
-# ::I18n.fallbacks.map(::LANG_FALLBACKS) # не работает
-::I18n.fallbacks = ::LANG_FALLBACKS
+# ::I18n.default_locale = :ru
+
+# ::I18n.fallbacks.map(::LANG_FALLBACKS)
+
+Rails.application.configure do
+  # fallbacks = ::LANG_FALLBACKS.each_with_object({}) do |(locale, fallback), result|
+  #   result[locale] = [locale, fallback].compact
+  #   result[locale] << :ru if fallback != :ru  # Добавляем :ru, если это не fallback
+  # end
+  # Задаём кастомные fallbacks
+  # https://guides.rubyonrails.org/v8.0.0/configuring.html#config-i18n-fallbacks
+  config.i18n.fallbacks.map = ::LANG_FALLBACKS
+end
